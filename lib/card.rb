a6380df265cdf404d35de10965c65271406b2b86
laurenjrichie <laurenjrichie@gmail.com>
2014-11-04 11:58:57 -0800

    initial commit

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,32 @@
 module BlackJack
   class Card
+
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+  end
+
+  def suit
+    @suit
+  end
+
+  def face
+    @face
+  end
+
+  def values
+    if face == "A"
+      [1, 11]
+    elsif face == "Q" || face == "K" || face == "J"
+      [10]
+    else
+      [face().to_i]
+    end
+  end
+
+  def ==(card_2)
+    face == card_2.face && suit == card_2.suit  # don't need card_1 in front of face or suit
+  end
+
   end
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,36 @@
 module BlackJack
   class Deck
+
+  def initialize
+
+    deck = []
+    suit = [:heart, :spade, :diamond, :club]
+    face = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"]
+
+    suit.each do |suit|
+        face.each do |face|
+          deck << BlackJack::Card.new(suit, face)
+        end
+    end
+
+    @deck = deck
+
+  end
+
+  def size
+    @deck.size
+  end
+
+  def cards
+    @deck
+  end
+
+  def shuffle
+  end
+
+  def deal
+    @deck.pop
+  end
+
   end
 end
